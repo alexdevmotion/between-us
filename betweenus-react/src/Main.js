4a2b8102c0027ec16735d68ea59fec87833425bc
Alexandru Constantin <alex@devmotion.ro>
2020-10-31 23:01:42 +0200

    fully functional webcam capture loop

--- betweenus-react/src/Main.js
+++ betweenus-react/src/Main.js
@@ -1,39 +1,43 @@
 import React from 'react';
-import Boundingbox from 'react-bounding-box';
 import Webcam from 'react-webcam';
+
+import Canvas from './Canvas';
+
+const b64toBlob = (b64Data, contentType = '', sliceSize = 512) => {
+  const byteCharacters = atob(b64Data);
+  const byteArrays = [];
+
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+    const byteNumbers = new Array(slice.length);
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byteNumbers);
+
+    byteArrays.push(byteArray);
+  }
+
+  return new Blob(byteArrays, {type: contentType});
+};
 
 class Main extends React.Component {
   imageFormat = 'image/jpeg';
-  captureLoopEnabled = true;
+  captureLoopEnabled = false;
   state = {
-    uploadedImageBase64: null,
+    imageBase64: null,
     boxes: [],
-    bboxOptions: {
-      colors: {
-        normal: 'rgba(255,225,255,1)',
-        selected: 'rgba(0,225,204,1)',
-        unselected: 'rgba(100,100,100,1)'
-      },
-      style: {
-        maxWidth: '100%',
-        maxHeight: '90vh'
-      },
-      base64Image: true,
-      // showLabels: true
-    },
-    uploading: true
   };
 
   constructor(props) {
     super(props);
-
     this.webcamRef = React.createRef();
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.startCaptureLoop();
-    }, 2000);
+    this.startCaptureLoop();
   }
 
   toBase64(file) {
@@ -41,8 +45,7 @@ class Main extends React.Component {
       const reader = new FileReader();
       reader.readAsDataURL(file);
       reader.onload = () => {
-        const base64Str = reader.result;
-        resolve(base64Str.split(',')[1]);
+        resolve(reader.result);
       }
       reader.onerror = error => reject(error);
     });
@@ -50,102 +53,83 @@ class Main extends React.Component {
 
   setBoxes(boxesResponse) {
     this.setState({
-      boxes: boxesResponse.boxes.map((bs, index) => ({
-        coord: [bs[0], bs[1], bs[2] - bs[0], bs[3] - bs[1]],
-        label: boxesResponse.too_close[index] ? 'BAD' : 'GOOD'
-      }))
+      boxes: boxesResponse.boxes.map((bs, index) => {
+        return {
+          coord: [bs[0], bs[1], bs[2] - bs[0], bs[3] - bs[1]],
+          label: boxesResponse.too_close[index]
+        };
+      })
     });
   }
 
-  async onFileChange(event) {
+  onFileChange(event) {
     const imageFile = event.target.files[0];
     if (imageFile) {
-      this.uploadFile(imageFile);
-      this.setState({
-        uploadedImageBase64: await this.toBase64(imageFile)
+      this.toBase64(imageFile).then(imageBase64 => {
+        this.computeAndDrawSocialDistance(imageFile, imageBase64);
       });
     }
   }
 
-  uploadFile(imageFile) {
+  computeAndDrawSocialDistance(imageFile, imageBase64) {
     const formData = new FormData();
     formData.append('image', imageFile);
-
-    this.setState({
-      uploading: true
-    });
 
     fetch(`http://localhost:8000/predict`, {
       method: 'POST',
       body: formData
     }).then(res => res.json())
       .then(res => {
+        this.setState({imageBase64});
         this.setBoxes(res);
-        this.setState({
-          uploading: false
-        });
         if (this.captureLoopEnabled) {
-          // setTimeout(() => {
-          //   this.onCapturePhoto();
-          // }, 1000);
+          this.onCapture();
         }
       });
   }
 
-  b64toBlob(b64Data, contentType, sliceSize) {
-    contentType = contentType || '';
-    sliceSize = sliceSize || 512;
+  onCapture() {
+    const restartLoopWithDelay = () => {
+      setTimeout(() => {
+        this.startCaptureLoop();
+      }, 1000);
+    };
 
-    const byteCharacters = atob(b64Data);
-    const byteArrays = [];
-
-    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-      const slice = byteCharacters.slice(offset, offset + sliceSize);
-
-      const byteNumbers = new Array(slice.length);
-      for (let i = 0; i < slice.length; i++) {
-        byteNumbers[i] = slice.charCodeAt(i);
-      }
-
-      const byteArray = new Uint8Array(byteNumbers);
-
-      byteArrays.push(byteArray);
+    if (!this.webcamRef.current) {
+      restartLoopWithDelay();
+      return;
     }
-
-    return new Blob(byteArrays, {type: contentType});
-  }
-
-  onCapturePhoto() {
     const captureBase64 = this.webcamRef.current.getScreenshot();
+    if (!captureBase64) {
+      restartLoopWithDelay();
+      return;
+    }
     const captureBase64Jpeg = captureBase64.split(',')[1];
+    const captureFile = b64toBlob(captureBase64Jpeg, this.imageFormat);
 
-    this.uploadFile(this.b64toBlob(captureBase64Jpeg, this.imageFormat));
-    this.setState({
-      uploadedImageBase64: captureBase64Jpeg
-    });
+    this.computeAndDrawSocialDistance(captureFile, captureBase64);
   }
 
   startCaptureLoop() {
     if (this.captureLoopEnabled) {
-      this.onCapturePhoto();
+      this.onCapture();
     }
   }
 
   render() {
     return <>
       <div>
-        <input type='file' onChange={this.onFileChange.bind(this)}/>
+        {!this.captureLoopEnabled && <input type='file' onChange={this.onFileChange.bind(this)}/>}
       </div>
       <div>
-        {this.state.uploadedImageBase64 &&
-        <Boundingbox image={this.state.uploadedImageBase64}
-                     boxes={this.state.boxes}
-                     options={this.state.bboxOptions}/>}
+        {this.state.imageBase64 &&
+        <Canvas background={this.state.imageBase64}
+                boxes={this.state.boxes}/>}
       </div>
       <div>
-        <Webcam audio={false}
+        {this.captureLoopEnabled && <Webcam audio={false} style={{visibility: 'hidden'}}
                 ref={this.webcamRef}
-                screenshotFormat={this.imageFormat}/>
+                screenshotFormat={this.imageFormat}/>}
       </div>
     </>
   }
