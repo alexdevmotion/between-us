a0f8df64195a87fcef9f51e75adc745e45f22a2b
Alexandru Constantin <alex@devmotion.ro>
2020-10-31 23:00:19 +0200

    improved social distance computation

--- betweenus-api/social_distance.py
+++ betweenus-api/social_distance.py
@@ -2,7 +2,7 @@ import math
 import numpy as np
 
 
-def check_social_distance(centers, angles, idx, threshold=2):
+def check_social_distance(centers, angles, idx, threshold=1.5):
     """
     return flag of alert if social distancing is violated
     """
@@ -15,8 +15,8 @@ def check_social_distance(centers, angles, idx, threshold=2):
     for i in sorted_idxs[1:]:
 
         # First check for distance
-        if distances[i] > threshold:
-            return False
+        if distances[i] < threshold and i != idx:
+            return True
 
         # More accurate check based on orientation and future position
         elif check_social_distance_advanced((xx, centers[i][0]), (zz, centers[i][1]), (angle, angles[i])):
@@ -25,20 +25,20 @@ def check_social_distance(centers, angles, idx, threshold=2):
     return False
 
 
-def check_social_distance_advanced(xxs, zzs, angles, threshold=2):
+def check_social_distance_advanced(xxs, zzs, angles, lookahead=0.5):
     """
     Violation if same angle or in in front of the other
     Obtained by assuming straight line, constant velocity and discretizing trajectories
     """
     theta0 = angles[0]
     theta1 = angles[1]
-    steps = np.linspace(0, 2, 20)  # Discretization 20 steps in 2 meters
+    steps = np.linspace(0, lookahead, 20)
     xs0 = [xxs[0] + step * math.cos(theta0) for step in steps]
     zs0 = [zzs[0] - step * math.sin(theta0) for step in steps]
     xs1 = [xxs[1] + step * math.cos(theta1) for step in steps]
     zs1 = [zzs[1] - step * math.sin(theta1) for step in steps]
     distances = [math.sqrt((xs0[idx] - xs1[idx]) ** 2 + (zs0[idx] - zs1[idx]) ** 2) for idx, _ in enumerate(xs0)]
-    if np.min(distances) <= max(distances[0] / 1.5, threshold):
+    if np.min(distances) <= max(distances[0] / 1.5, 0.5):
         return True
     return False
 
